e1e6060ad8c3d37bc090853158815dcc5f0daebd
Sean D'Epagnier <sean@depagnier.com>
2014-01-16 13:53:27 +0800

    author url

--- src/WatchdogPrefsDialog.cpp
+++ src/WatchdogPrefsDialog.cpp
@@ -168,7 +168,5 @@ be triggered again later."),
 
 void WatchdogPrefsDialog::OnAboutAuthor( wxCommandEvent& event )
 {
-/*
-      wxLaunchDefaultBrowser(_T("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sean%40depagnier%2ecom&lc=US&item_name=watchdog&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest"));
-*/
+    wxLaunchDefaultBrowser(_T(ABOUT_AUTHOR_URL));
 }
