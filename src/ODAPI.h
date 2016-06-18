36e420a03869e3ab2a6b70d68a07658e8257e87d
Jon Gough <jon.gough@eclipsesystems.com.au>
2016-06-18 17:46:18 +1000

    Add OD API direct access

---
+++ src/ODAPI.h
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * $Id: ODAPI.h,v 1.0 2016/06/17 01:54:37 jongough Exp $
+ *
+ * Project:  OpenCPN
+ * Purpose:  OpenCPN General Drawing Plugin API
+ * Author:   Jon Gough
+ *
+ ***************************************************************************
+ *   Copyright (C) 2010 by David S. Register                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ **************************************************************************/
+
+#ifndef _ODAPI_H_
+#define _ODAPI_H_
+
+#ifndef DECL_EXP
+#ifdef __WXMSW__
+#  define DECL_EXP     __declspec(dllexport)
+#else
+# ifdef __GNUC__
+# define DECL_EXP       __attribute__((visibility("default")))
+# endif
+#endif
+#endif
+
+class ODAPI
+{
+    public:
+        ODAPI();
+        ~ODAPI();
+        static bool OD_FindPathByGUID( wxString sGUID, wxString sName, wxString sDescription );
+    protected:
+    
+    private:
+};
+
+typedef bool (*OD_FindPathByGUID) (wxString, wxString *, wxString *);
+
+
+#endif //_ODAPI_H_
