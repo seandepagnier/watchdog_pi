859bc8bcc9da544d4bd3cdd3b056bcdc734e08ba
jon <jon.gough@eclipsesystems.com.au>
2017-01-07 15:11:55 +1100

    Add Properties dialog box. Only shows an about box currently

---
+++ include/WatchdogPropertiesDialog.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+*
+* Project:  OpenCPN
+* Purpose:  Watchdog Properties Dialog support
+* Author:   Jon Gough
+*
+***************************************************************************
+*   Copyright (C) 2010 by David S. Register                               *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+**************************************************************************/
+
+#ifndef __WatchdogPropertiesDialog__
+#define __WatchdogPropertiesDialog__
+
+#include "WatchdogUI.h"
+#include <wx/bmpcbox.h>
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+class WatchdogPropertiesDialog : public WatchdogPropertiesDialogBase
+{
+protected:
+    void OnWatchdogPropertiesOKClick( wxCommandEvent& event );
+    
+public:
+    WatchdogPropertiesDialog( wxWindow* parent );
+    
+private:
+    
+};
+
+#endif // __WatchdogPropertiesDialog__
