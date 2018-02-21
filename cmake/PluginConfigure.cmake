d50debdcb74ca63d705fdc777b8cc1450720f0c6
Sean D'Epagnier <seandepagnier@gmail.com>
2018-02-20 21:27:21 -0500

    do not hardcode optimizations (-O2 is already added for release builds)

--- cmake/PluginConfigure.cmake
+++ cmake/PluginConfigure.cmake
@@ -26,7 +26,7 @@ IF(NOT MSVC)
   ADD_DEFINITIONS( "-Wall -g -fprofile-arcs -ftest-coverage -fexceptions" )
  ELSE(PROFILING)
 #  ADD_DEFINITIONS( "-Wall -g -fexceptions" )
-  ADD_DEFINITIONS( "-Wall -Wno-unused-result -g -O2 -fexceptions -fPIC" )
+  ADD_DEFINITIONS( "-Wall -Wno-unused-result -g -fexceptions -fPIC" )
  ENDIF(PROFILING)
 
  IF(NOT APPLE)
