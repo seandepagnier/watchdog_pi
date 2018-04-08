40a50c9b203ed21bb24da6a542d087d3a63e38b3
didier <dgautheron@magic.fr>
2018-04-08 17:33:05 +0200

    set visibility hidden

--- cmake/PluginConfigure.cmake
+++ cmake/PluginConfigure.cmake
@@ -22,6 +22,7 @@ INCLUDE_DIRECTORIES(BEFORE ${PROJECT_SOURCE_DIR}/include ${PROJECT_SOURCE_DIR}/s
 
 #  IF NOT DEBUGGING CFLAGS="-O2 -march=native"
 IF(NOT MSVC)
+ ADD_DEFINITIONS( "-fvisibility=hidden" )
  IF(PROFILING)
   ADD_DEFINITIONS( "-Wall -g -fprofile-arcs -ftest-coverage -fexceptions" )
  ELSE(PROFILING)
