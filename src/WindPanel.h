793c5c1ecee76c8b799130750805caa9189d1767
Sean D'Epagnier <seandepagnier@gmail.com>
2018-05-22 09:05:37 -0400

    add missing file

---
+++ src/WindPanel.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *
+ * Project:  OpenCPN
+ * Purpose:  watchdog Plugin
+ * Author:   Sean D'Epagnier
+ *
+ ***************************************************************************
+ *   Copyright (C) 2018 by Sean D'Epagnier                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ */
+
+class WindPanel : public WindPanelBase
+{
+public:
+    WindPanel( wxWindow* parent, double &direction) :
+    WindPanelBase(parent), m_direction(direction) {}
+    void OnType( wxCommandEvent& );
+    void OnAboutWind( wxCommandEvent& );
+    void OnSync( wxCommandEvent& );
+
+    double &m_direction;
+};
