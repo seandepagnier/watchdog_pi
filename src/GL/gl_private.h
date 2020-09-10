a411358b0175cd9b870074bcd561e45c95d82b65
dsr <bdbcat@yahoo.com>
2020-09-09 22:33:48 -0400

    Update glshim for Android build

--- src/GL/gl_private.h
+++ src/GL/gl_private.h
@@ -54,7 +54,23 @@
 extern "C" {
 #endif
 
-typedef double          GLclampd;       /* double precision float in [0,1] */
+//typedef double          GLclampd;       /* double precision float in [0,1] */
+typedef unsigned int	GLenum;
+typedef unsigned char	GLboolean;
+typedef unsigned int	GLbitfield;
+typedef void		GLvoid;
+typedef signed char	GLbyte;		/* 1-byte signed */
+typedef short		GLshort;	/* 2-byte signed */
+typedef int		GLint;		/* 4-byte signed */
+typedef unsigned char	GLubyte;	/* 1-byte unsigned */
+typedef unsigned short	GLushort;	/* 2-byte unsigned */
+typedef unsigned int	GLuint;		/* 4-byte unsigned */
+typedef int		GLsizei;	/* 4-byte signed */
+typedef float		GLfloat;	/* single precision float */
+typedef float		GLclampf;	/* single precision float in [0,1] */
+typedef double		GLdouble;	/* double precision float */
+typedef double		GLclampd;	/* double precision float in [0,1] */
+typedef long  GLsizeiptr;
 
 // These two definitions need to be present for Qt 5.5, but removed for Qt 5.3
 
