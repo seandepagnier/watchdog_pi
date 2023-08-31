049eb28d50d93cf51f81bcc18a40c9020e89e479
Dave <bdbcat@yahoo.com>
2023-08-31 14:31:00 -0400

    Test build #3

--- include/ConfigurationDialog.h
+++ include/ConfigurationDialog.h
@@ -30,14 +30,6 @@
 #include "GL/gl.h"            // local copy for Windows
 #include <GL/glu.h>
 #else
-
-#ifndef __OCPN__ANDROID__
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-#else
-//#include "qopengl.h"                  // this gives us the qt runtime gles2.h
-//#include "GL/gl_private.h"
-#endif
 #endif
 
 
--- src/watchdog_pi.cpp
+++ src/watchdog_pi.cpp
@@ -26,7 +26,6 @@
 
 #include <wx/wx.h>
 #include <wx/stdpaths.h>
-//#include <GL/gl.h>
 
 #include "json/json.h"
 
